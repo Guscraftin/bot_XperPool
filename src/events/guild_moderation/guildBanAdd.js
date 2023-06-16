5f7ad5b719986d89c38c07d7ba2a57afb8c1702a
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-06-16 17:14:38 +0200

    feature: add ban/unban logs

---
+++ src/events/guild_moderation/guildBanAdd.js
@@ -0,0 +1,32 @@
+const { Events, EmbedBuilder } = require('discord.js');
+const { channel_logs_moderation } = require('../../const.json');
+
+module.exports = {
+    name: Events.GuildBanAdd,
+    async execute(ban){
+
+        /*
+         * Log
+         */
+        try {
+            await ban.fetch();
+
+            const embed = new EmbedBuilder()
+                .setTitle(`Banissement`)
+                .setColor('#009ECA')
+                .setDescription(`**\`${ban.user.username}\` a été banni.**
+                > **Id :** \`${ban.user.id}\`
+                > **Raison :** \`${ban.reason === null ? `Aucune raison fourni` : `${ban.reason}`}\`
+                `)
+                .setThumbnail(ban.user.displayAvatarURL())
+                .setTimestamp()
+                .setFooter({ text: ban.guild.name, iconURL: ban.guild.iconURL() })
+
+            ban.guild.channels.fetch(channel_logs_moderation).then(channel =>
+                channel.send({ embeds: [embed] })
+            );
+        } catch (error) {
+            console.error(error);
+        }
+    }
+};
---
+++ src/events/guild_moderation/guildBanRemove.js
@@ -0,0 +1,30 @@
+const { Events, EmbedBuilder } = require('discord.js');
+const { channel_logs_moderation } = require('../../const.json');
+
+module.exports = {
+    name: Events.GuildBanRemove,
+    async execute(ban){
+
+        /*
+         * Log
+         */
+        try {
+            const embed = new EmbedBuilder()
+                .setTitle(`Débanissement`)
+                .setColor('#009ECA')
+                .setDescription(`**\`${ban.user.username}\` a été débanni.**
+                > **Id :** \`${ban.user.id}\`
+                > **Raison :** \`${ban.reason === null ? `Aucune raison fourni` : `${ban.reason}`}\`
+                `)
+                .setThumbnail(ban.user.displayAvatarURL())
+                .setTimestamp()
+                .setFooter({ text: ban.guild.name, iconURL: ban.guild.iconURL() })
+            
+            ban.guild.channels.fetch(channel_logs_moderation).then(channel =>
+                channel.send({ embeds: [embed] })
+            );
+        } catch (error) {
+            console.error(error);
+        }
+    }
+};
