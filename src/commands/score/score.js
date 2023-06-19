8eda0e3c7d3316f222652f7eae6df0ff76ddaae2
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-06-19 17:48:22 +0200

    feature: add score cmd - check the score of a user

---
+++ src/commands/score/score.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { Op } = require('sequelize');
+const { Members } = require('../../dbObjects');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("score")
+		.setDescription("Affiche le score d'un membre.")
+        .setDMPermission(false)
+        .addUserOption(option => option.setName('membre').setDescription("Le membre dont afficher le score.")),
+	async execute(interaction) {
+        try {
+            const member = interaction.options.getUser("membre") || interaction.user;
+            const user = await Members.findOne({ where: { member_id: member.id } });
+            if (!user) return interaction.reply({ content: "Ce membre n'a pas de score.", ephemeral: true });
+            const totalMembers = await Members.count();
+            const rank = await Members.count({ where: { score: { [Op.gt]: user.score } } }) + 1;
+
+            const embed = new EmbedBuilder()
+                .setTitle(`Stats de ${member.username}`)
+                .setDescription(`Score : \`${user.score}\`\n`+
+                `Rang : \`${rank}/${totalMembers}\``);
+
+            return interaction.reply({ embeds: [embed], ephemeral: true });
+        } catch (error) {
+            console.error("score.js - " + error);
+        }
+	},
+};
