4c6e386af174772640141aa3b0f61e48777b0827
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-06-27 07:57:48 +0200

    fix: messageCreate - remove create user

--- src/events/guild_messages/messageCreate.js
+++ src/events/guild_messages/messageCreate.js
@@ -35,24 +35,15 @@ module.exports = {
             if (message.channel.type === 'DM') return;
 
             const member = await Members.findOne({ where: { member_id: message.author.id } });
-            if (!member) {
-                const force = process.argv.includes('--force') || process.argv.includes('-f');
-                await sequelize.sync({ force }).then(async () => {
-                    const memberDB = [
-                        Members.upsert({ member_id: message.author.id, score: 1 }),
-                    ];
-                
-                    await Promise.all(memberDB);
-                }).catch(console.error);
-            } else {
+            // Only if the member exist in the database (he passed the verification)
+            if (member) {
                 // Check if member has send a message in the last 5 seconds
                 if (!member.is_messaging) {
                     // Update member score
                     await member.update({ score: member.score + 1, is_messaging: true });
                     setTimeout(async () => {
                         await member.update({ is_messaging: false });
-                    }
-                    , 5000);
+                    }, 5000);
                 }
             }
         } catch (error) {
