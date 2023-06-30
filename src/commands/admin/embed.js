c740ec455b339eaefb22e5c44e5ae427185ea34b
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-06-30 08:26:32 +0200

    updt: embed - update msg

--- src/commands/admin/embed.js
+++ src/commands/admin/embed.js
@@ -12,7 +12,8 @@ module.exports = {
         .addStringOption(option => option.setName("description").setDescription("La description de l'embed."))
         .addAttachmentOption(option => option.setName("vignette").setDescription("La \"miniatur`\" de l'embed."))
         .addStringOption(option => option.setName("couleur").setDescription("La couleur de l'embed (format html comme : #77Ab00).").setMinLength(6).setMaxLength(7))
-        .addAttachmentOption(option => option.setName("image").setDescription("L'image de l'embed.")),
+        .addAttachmentOption(option => option.setName("image").setDescription("L'image de l'embed."))
+        .addStringOption(option => option.setName("id").setDescription("L'id du message a modifier dans ce salon.")),
     async execute(interaction) {
         const content = interaction.options.getString("contenu");
         const titre = interaction.options.getString("titre");
@@ -21,16 +22,23 @@ module.exports = {
         const vignette = interaction.options.getAttachment("vignette");
         const color = interaction.options.getString("couleur");
         const image = interaction.options.getAttachment("image");
+        const id = interaction.options.getString("id");
 
         // Exception if some required options are missing
         if (url && !titre) return interaction.reply({ content: "Vous devez spécifier un titre si vous spécifiez une url.", ephemeral: true });
-        let vignetteUrl, imageUrl;
+        let vignetteUrl, imageUrl, msg;
         if (vignette) vignetteUrl = vignette.url;
         if (image) imageUrl = image.url;
         if (color && !titre && !description && !vignetteUrl && !imageUrl) return interaction.reply({ content: "Vous devez spécifier au moins un titre, une description, une vignette ou une image si vous spécifiez une couleur.", ephemeral: true });
         if (color) {
             const pattern = /^(#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})|([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3}))$/;
             if (!pattern.test(color)) return interaction.reply({ content: "La couleur doit être au format html (comme : #77Ab00).", ephemeral: true });
+        }
+        if (id) {
+            msg = await interaction.channel.messages.fetch(id).catch(() => { return null; });
+            if (!msg || msg.size > 1) return interaction.reply({ content: "Le message à modifier n'existe pas.", ephemeral: true });
+            if (!msg.author.bot) return interaction.reply({ content: "Le message à modifier n'a pas été envoyé par le bot.", ephemeral: true });
+            if (!content && !titre && !description && !vignetteUrl && !imageUrl) return interaction.reply({ content: "Vous devez spécifier au moins un contenu, un titre, une description, une vignette ou une image.\nSi vous souhaitez supprimer le message, supprimez le manuellement.", ephemeral: true });
         }
 
         // Send the message
@@ -42,8 +50,14 @@ module.exports = {
             .setColor(color)
             .setImage(imageUrl);
 
-        if (content) await interaction.channel.send({ content: content, embeds: [embed] });
-        else await interaction.channel.send({ embeds: [embed] });
-        await interaction.reply({ content: "Message correctement envoyé !", ephemeral: true });
+        if (id) {
+            if (!titre && !description && !vignetteUrl && !imageUrl) await msg.edit({ content: content, embeds: [] });
+            else await msg.edit({ content: content, embeds: [embed] });
+            await interaction.reply({ content: `Message correctement modifié : ${msg.url} !`, ephemeral: true });
+        } else {
+            if (content) await interaction.channel.send({ content: content, embeds: [embed] });
+            else await interaction.channel.send({ embeds: [embed] });
+            await interaction.reply({ content: "Message correctement envoyé !", ephemeral: true });
+        }
     },
 };
