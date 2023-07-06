6ee2ed96d1c24efe7e189201115746f48c559e49
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-07-06 18:07:13 +0200

    fix: guildMemberRemove - remove kick or ban option

--- src/events/guild_members/guildMemberRemove.js
+++ src/events/guild_members/guildMemberRemove.js
@@ -8,41 +8,6 @@ module.exports = {
         /*
          * Log
          */
-        // Check if the member has been kicked
-        const fetchKickLog = await member.guild.fetchAuditLogs({
-            limit: 1,
-            type: 20
-        });
-
-        let kickLog, targetKick, reasonKick, createdAt;
-        if (fetchKickLog.entries.size > 0) {
-            kickLog = fetchKickLog.entries.first();
-            targetKick = kickLog.target;
-            reasonKick = kickLog.reason;
-            createdAt = kickLog.createdAt;
-        }
-        let isMemberKick = false;
-        let isMemberBan = false;
-        let reasonBan;
-
-        if (kickLog && targetKick.id === member.id && Date.now() - createdAt.getTime() < 0) isMemberKick = true;
-        else {
-            // Check if the member has been banned
-            const fetchBanLog = await member.guild.fetchAuditLogs({
-                limit: 1,
-                type: 22
-            });
-
-            let banLog, targetBan, reasonBan;
-            if (fetchBanLog.entries.size > 0) {
-                banLog = fetchBanLog.entries.first();
-                targetBan = banLog.target;
-                reasonBan = banLog.reason;
-            }
-            if (banLog && targetBan.id === member.id && Date.now() - banLog.createdTimestamp < 0) isMemberBan = true;
-        }
-
-        // Send the log message
         const embed = new EmbedBuilder()
             .setAuthor({ name: `${member.user.username} (${member.id})`, iconURL: member.user.displayAvatarURL() })
             .setColor('#dc143c')
@@ -50,9 +15,7 @@ module.exports = {
 `• Nom d'utilisateur : ${member.displayName} - \`${member.user.username}\` (${member.id})
 • Créé le : <t:${parseInt(member.user.createdTimestamp / 1000)}:f> (<t:${parseInt(member.user.createdTimestamp / 1000)}:R>)
 • Rejoint le : <t:${parseInt(member.joinedTimestamp / 1000)}:f> (<t:${parseInt(member.joinedTimestamp / 1000)}:R>)
-• Quitté le : <t:${parseInt(Date.now() / 1000)}:f> (<t:${parseInt(Date.now() / 1000)}:R>)` +
-`${isMemberKick ? `\n• Expulsé : 🟢 (raison : ${reasonKick !== null ? reasonKick : `\`Non fournie\``})` : ``}` +
-`${isMemberBan ? `\n• Banni : 🟢 (raison : ${reasonBan !== null ? reasonBan : `\`Non fournie\``})` : ``}`)
+• Quitté le : <t:${parseInt(Date.now() / 1000)}:f> (<t:${parseInt(Date.now() / 1000)}:R>)`)
             .setTimestamp()
             .setFooter({ text: `L'utilisateur a quitté !` })
 
