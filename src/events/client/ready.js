20d61a747d6440ab83cca127bd04d7ca89249497
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-07-07 11:20:06 +0200

    feat: send DM to field the form in every 48h

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,5 +1,5 @@
-const { Events } = require('discord.js');
-const { channel_detail_missions } = require(process.env.CONST);
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, Events } = require('discord.js');
+const { channel_detail_missions, color_basic } = require(process.env.CONST);
 const { Items, Members, Missions, LogMissions, Tickets } = require('../../dbObjects');
 
 module.exports = {
@@ -20,25 +20,85 @@ module.exports = {
             await LogMissions.sync({ alter: true });
             await Tickets.sync({ alter: true });
         } catch (error) {
-            console.error("ready.js - " + error);
+            console.error("ready.js syncDB - " + error);
         }
 
         // Reinitialize is_messaging to false for all members
         await Members.update({ is_messaging: false }, { where: { is_messaging: true } });
 
         // Delete all threads for a deleted mission
-        const threads = await client.channels.fetch(channel_detail_missions).then(channel => {return channel.threads.fetch()});
+        const threads = await client.channels.fetch(channel_detail_missions).then(channel => { return channel.threads.fetch() });
         await threads.threads.filter(thread => thread.parentId === channel_detail_missions).forEach(async thread => {
-            const message = await thread.messages.fetch().then(messages => {return messages.first()});
+            const message = await thread.messages.fetch().then(messages => { return messages.first() });
             if (message.content.includes("mission a été supprimé")) {
                 await thread.delete();
             }
         });
 
+        // Start the function calledEvery48h for sending DM to members not in the database
+        await calledEvery48h(sendDM, client);
+
         // Send a message when the bot is ready
         console.log(`${client.user.username} est prêt à être utilisé par ${usersCount} utilisateurs sur ${guildsCount.size} serveurs !`);
     },
 };
+
+
+/**
+ * Call a function every 48 hours
+ */
+async function calledEvery48h(callback, client) {
+    // Duration in milliseconds (48 hours)
+    const duration = 48 * 60 * 60 * 1000;
+
+    // Call the first time the callback function
+    await callback(client);
+
+    // Call the callback function every 48 hours
+    setInterval(callback, duration, client);
+}
+
+/**
+ * Send a DM message to all members who are not in the database
+ */
+async function sendDM(client) {
+    // Get all members of the server
+    const guild = await client.guilds.fetch(process.env.GUILD_ID);
+    const members = await guild.members.fetch();
+
+    // Get all members in the database
+    const membersInDB = await Members.findAll();
+
+    // Get all members who are not in the database
+    const membersNotInDB = members.filter(
+        (member) => !membersInDB.some((m) => m.member_id === member.id)
+    );
+
+    // Send a DM message to all members who are not in the database
+    membersNotInDB.forEach(async (member) => {
+        if (member.user.bot) return;
+
+        const embed = new EmbedBuilder()
+            .setDescription(`## Bonjour ${member} !\nJ'ai remarqué que vous n'avez pas encore remplit le formulaire d'accès qui vous permettra de profiter de toutes les fonctionnalités du serveur ${guild.name}.\n\nL'équipe administrative du projet est impatiente de recevoir votre réponse au formulaire suivant : **https://tally.so/r/3EKPvN**.`)
+            .setColor(color_basic)
+            .setThumbnail('https://media.discordapp.net/attachments/1117544311848976505/1117544390534119554/Xperpool_logo_2.jpeg?width=225&height=225')
+            .setFooter({ text: "A tout de suite dans le formulaire !" })
+
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setLabel('Lien vers le formulaire')
+                    .setStyle(ButtonStyle.Link)
+                    .setURL('https://tally.so/r/3EKPvN')
+            );
+
+        await member.send({ embeds: [embed], components: [row] }).catch(() => {});
+    });
+}
+
+
+
+
 
 
 //////////////////////////////////////////////////////////////////////////////////////////
