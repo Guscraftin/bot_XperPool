0f208d1ebc8ac08e2cd18e1bab707e17f08618c2
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-07-11 14:58:37 +0200

    updt: msg in welcome channel

--- src/commands/admin/docs.js
+++ src/commands/admin/docs.js
@@ -58,32 +58,27 @@ module.exports = {
 
                 const embed = new EmbedBuilder()
                     .setAuthor({ name: `${owner.displayName}`, iconURL: owner.displayAvatarURL() })
-                    .setTitle("La Communauté d'Experts !")
-                    .setColor(656268)
+                    .setTitle("👋 Bienvenue dans la Communauté d'Experts XperPool !")
+                    .setColor(color_basic)
                     .setThumbnail('https://images-ext-2.discordapp.net/external/sHMmkyKqLTBXwgigTcXBA0TCHhdTa4rIBfDt15wdWtA/https/cdn-longterm.mee6.xyz/plugins/embeds/images/1079366462050934784/bf7b417c7216b32a517d0a4abaa8679b74f15b334a800b49c16890b486b8e1cf.png?width=253&height=253')
-                    .setDescription(`Bienvenue dans le réseau XperPool, nous sommes très heureux de vous accueillir sur ce serveur Discord dédié au Portage Salarial IT 😉
+                    .setDescription(`_ _
+Remplissez rapidement notre court questionnaire pour rejoindre officiellement la communauté et accéder à des salons exclusifs ("missions-disponibles","astuces",...) :
 
-Afin de pouvoir rejoindre officiellement la communauté et accéder aux différents salons, allez vite remplir notre petit questionnaire pour récupérer votre accès !
+🔗 **[Lien du questionnaire : https://tally.so/r/3EKPvN]**
 
-https://tally.so/r/3EKPvN
+Connectez-vous avec des professionnels de divers secteurs, partagez vos expériences et posez des questions sur n'importe quel sujet. Explorez nos missions adaptées à votre profil, __**que vous soyez salarié ou freelance**__, pour booster votre carrière et développer vos compétences ! 
 
-Ici, vous pourrez discuter avec de nombreux professionnels de diverses secteurs, partager vos expériences et poser un tas de questions sur n'importe quel domaine.
-
-Nous proposons également des missions adaptées à votre profil pour vous aider à développer votre carrière et votre montée en compétences !
-
-N'hésitez pas à consulter les différentes sections du serveur, en commençant par le règlement, et à participer aux discussions.
-
-L'équipe XperPool vous souhaite à tous la bienvenue 😃`)
+L'équipe XperPool vous souhaite à tous la bienvenue ! 😃`)
                     .setImage("https://images-ext-1.discordapp.net/external/bpXrVyDFBNagVkp4fdSrb18PDhhq7xipFJdhkzkZXaQ/https/cdn-longterm.mee6.xyz/plugins/embeds/images/1079366462050934784/0b9e7285ca69b649abe7e9dded54bc7e452384a92d5359d92075d2ccf731fa01.png?width=866&height=214")
 
 
                 const row = new ActionRowBuilder()
                     .addComponents(
                         new ButtonBuilder()
-                            .setLabel("Site Web")
+                            .setLabel("Questionnaire")
                             .setStyle(ButtonStyle.Link)
                             .setEmoji('1079367441387356200')
-                            .setURL("https://xperpool.fr/"),
+                            .setURL("https://tally.so/r/3EKPvN"),
                     );
 
                 const msg1 = await interaction.channel.send({ embeds: [embed], components: [row] });
